fd028dfaa288fc82eb6f483b83dfba5d3d2b89b2
Henry Ji <jeep.jipu@gmail.com>
2025-06-19 22:11:22 +0800

    设置LocalDateTime使用ISO格式

--- backend/web/src/main/java/com/morningstar/util/JsonUtil.java
+++ backend/web/src/main/java/com/morningstar/util/JsonUtil.java
@@ -3,21 +3,40 @@ package com.morningstar.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 public class JsonUtil {
+    private static SimpleModule getLongModule(){
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, new ToStringSerializer());
+        module.addSerializer(Long.TYPE, new ToStringSerializer());
+        return module;
+    }
+
+    private static SimpleModule getLocalDateTimeModule(){
+        SimpleModule module = new SimpleModule();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+        module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(formatter));
+        module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(formatter));
+        return module;
+    }
+
     public static ObjectMapper objectMapper(){
         ObjectMapper mapper = new ObjectMapper();
 //        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY); // 不推荐，存在安全风险, @JsonIgnore失效
 
         // (支持Java8的日期时间类型)
         mapper.findAndRegisterModules();
-
         // 设置Long的序列化
-        SimpleModule module = new SimpleModule();
-        module.addSerializer(Long.class, new ToStringSerializer());
-        module.addSerializer(Long.TYPE, new ToStringSerializer());
-        mapper.registerModule(module);
+        mapper.registerModule(getLongModule());
+        // 设置LocalDateTime的序列化与反序列化
+        mapper.registerModule(getLocalDateTimeModule());
 
         return mapper;
     }
